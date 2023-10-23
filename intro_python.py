7858e3652e51079aa46b0d3855ed17d8f8e29c64
Luisa <lfbetancourta@distrital.edu.co>
2023-10-23 16:05:54 -0500

    Primera

---
+++ intro_python.py
@@ -0,0 +1,62 @@
+#git remote add origin https://github.com/luisa0525/python_basic.git
+
+
+#comentario de una sola linea
+'''
+comentario de varias lineas
+'''
+'''
+variables:
+texto
+enteros, flotantes
+letra
+'''
+nombre= 3.12
+otra= "hola"
+tercera=45
+otraotra=8.6
+pass     # ejecute esa linea y no haga nada más
+
+# funciones en python:
+
+# ¿qué son?
+# Mecanismos de organización del codigo fuente en donde agrupamos un conjunto de operaciones (sentencias) que cumplen un objetivo especifico.
+# La funcion de le asigna un nombre
+# Puede aceptar uno o varios valores para hacer su trabajo
+# Puede retornar a quien la llama uno o varios valores
+
+
+print(type(nombre))
+print(type(otra))
+print(type(tercera))
+
+
+'''
+print("hola mundo")
+print(nombre)
+'''
+print(nombre+tercera)
+
+# cast: promover, promocionar, llevar algo a otro nivel, elevar.
+
+# str: convierte a cadena de texto 
+
+print(otra+str(tercera))
+
+print(int(otraotra))
+
+
+# matriz de variables: conjunto de variables organizadas en filas y columnas
+
+# usando listas crear una matriz de 8x8.
+# crear un programa en python que de manera aleatoria llene esa matriz.
+# visualizar el canal rojo, verde y azul.
+# implementar un filtro que pase la imagen a color a escala de grises.
+# crear la imagen aleatoria.
+
+
+
+
+
+
+
